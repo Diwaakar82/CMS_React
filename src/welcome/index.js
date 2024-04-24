024bf737944522d547ccec3199867ea4e7b7f521
Diwaakar82 <diwaakar@mobicip.com>
2024-04-24 15:55:05 +0530

    Modified homepage

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -1,22 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../shared/auth';
 import "./index.css"
 
 function Home() {
-  const auth = useContext(AuthContext);
-  const storedData = JSON.parse(localStorage.getItem('userData'));
-
   return (
     <div className="landing_page">
       <h1>Content Management System</h1>
       <Link to="/categories" className="welcome_button">Categories</Link>
 
       <Link to="/posts" className="welcome_button">Posts</Link>
-
-      { auth.isLoggedIn &&
-          (<><Link to={`/users/${storedData['userId']}/my_posts`} className="welcome_button">My Posts</Link></>)
-      }
     </div>
   );
 }
